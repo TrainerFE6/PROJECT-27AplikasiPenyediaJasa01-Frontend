a87cec0635ed4a9c3639fe3d099488b79ff4b939
GamelabAhmad <164120476+GamelabAhmad@users.noreply.github.com>
2024-06-21 10:31:39 +0700

    Update database.js

--- backend/library/database.js
+++ backend/library/database.js
@@ -1,32 +1,16 @@
 
-// let mysql = require('mysql');
+let mysql = require('mysql');
 // konfigurasi untuk koneksi database MySql
 
-// let connection = mysql.createConnection({
-//      host: 'educalab.id',
-//      user: 'PVs539vwtNMt7Ncb',
-//      password: 'qfJ6wzIyecQBWPMN',
-//      database: 'YsSqE2MU7M9rlns5',
-//      port: 3307
-//     });
+let connection = mysql.createConnection({
+     host: 'educalab.id',
+     user: 'PVs539vwtNMt7Ncb',
+     password: 'qfJ6wzIyecQBWPMN',
+     database: 'YsSqE2MU7M9rlns5',
+     port: 3307
+    });
 
 // Kondisi untuk mengecek database berjalan atau tidak
-// connection.connect(function(error){
-//  if(!!error){
-//  console.log(error);
-//  }else{
-//  console.log('Koneksi ke database MySql Berhasil!');
-//  }
-// })
-// module.exports = connection;
-
-let mysql = require('mysql');
-let connection = mysql.createConnection({
- host: 'localhost',
- user: 'root',
- password: '',
- database: 'service-provider'
-});
 connection.connect(function(error){
  if(!!error){
  console.log(error);
@@ -35,4 +19,20 @@ connection.connect(function(error){
  }
 })
 module.exports = connection;
+
+// let mysql = require('mysql');
+// let connection = mysql.createConnection({
+//  host: 'localhost',
+//  user: 'root',
+//  password: '',
+//  database: 'service-provider'
+// });
+// connection.connect(function(error){
+//  if(!!error){
+//  console.log(error);
+//  }else{
+//  console.log('Koneksi ke database MySql Berhasil!');
+//  }
+// })
+// module.exports = connection;
 
