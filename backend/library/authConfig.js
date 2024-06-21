42a30cbf3910f530f69f2daabc44b632fd05076f
GamelabAhmad <164120476+GamelabAhmad@users.noreply.github.com>
2024-06-21 10:50:51 +0700

    Update authConfig.js

--- backend/library/authConfig.js
+++ backend/library/authConfig.js
@@ -28,10 +28,15 @@ const mysql = require('mysql2/promise');
 
 // konfigurasi untuk koneksi database MySQL
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'service-provider',
+    // host: 'localhost',
+    // user: 'root',
+    // password: '',
+    // database: 'service-provider',
+    host: 'educalab.id',
+     user: 'PVs539vwtNMt7Ncb',
+     password: 'qfJ6wzIyecQBWPMN',
+     database: 'YsSqE2MU7M9rlns5',
+     port: 3307,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
